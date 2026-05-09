a65c69d9b419bbc693b489187e10caa588de15ba
Jackson Morgan <jaxoncreed@gmail.com>
2026-05-09 15:17:22 -0400

    Delete old and add CLAUDE.md

--- _old/plugins/dataPlugin/nemaline/index.ts
+++
@@ -1,8 +0,0 @@
-import { oxfordiaSchema } from "../../_ldo/oxfordia.schema";
-import { nemalineGraphPathShortcuts } from "./shortcuts";
-
-export const nemalineDataPlugin = {
-  name: "nemaline",
-  schema: oxfordiaSchema,
-  graphPathShortcuts: nemalineGraphPathShortcuts,
-};
--- _old/server/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./ApiHandler";
-export * from "./account/CreatePasswordLoginHandler";
-export * from "./account/CreatePromptFactory";
